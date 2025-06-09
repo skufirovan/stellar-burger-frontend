15a9cb45c74da2038d5d9b6c09c156682e51c053
Khaspalaev Anush <khaspalaevan05@gmail.com>
2025-06-09 23:36:36 +0300

    feat: implement feed refresh button

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,16 +1,19 @@
-import { useSelector } from '@store';
-import { ordersSelector } from '@slices/burgerSlice';
+import { useSelector, useDispatch } from '@store';
+import { fetchFeeds, ordersSelector } from '@slices/burgerSlice';
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
 import { FC } from 'react';
 
 export const Feed: FC = () => {
+  const dispatch = useDispatch();
   const orders: TOrder[] = useSelector(ordersSelector);
 
   if (!orders.length) {
     return <Preloader />;
   }
 
-  return <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  return (
+    <FeedUI orders={orders} handleGetFeeds={() => dispatch(fetchFeeds())} />
+  );
 };
